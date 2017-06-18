b63c6da0229737f67baab20b936c223f1ce42608
Anak Mirasing <thaihooligan@hotmail.com>
2017-06-18 12:54:00 +0700

    Prepare storyboard and viewcontroller class

---
+++ Movi/ViewController/MovieDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDetailViewController.swift
+//  Movi
+//
+//  Created by Anak Mirasing on 6/18/17.
+//  Copyright © 2017 iGROOMGRiM. All rights reserved.
+//
+
+import Foundation
+
+class MovieDetailViewController: TableViewController {
+    
+}
---
+++ Movi/ViewController/MovieListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MovieListViewController.swift
+//  Movi
+//
+//  Created by Anak Mirasing on 6/18/17.
+//  Copyright © 2017 iGROOMGRiM. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: TableViewController {
+    
+}
