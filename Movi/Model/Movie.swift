45ba5445c26dd28406ffd126787dcea51fbf6e10
Anak Mirasing <thaihooligan@hotmail.com>
2017-06-18 13:04:52 +0700

    Create movie model

---
+++ Movi/Model/Movie.swift
@@ -0,0 +1,47 @@
+//
+//  Movie.swift
+//  Movi
+//
+//  Created by Anak Mirasing on 6/18/17.
+//  Copyright © 2017 iGROOMGRiM. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class Movie: Mappable {
+    var voteCount: Int64?
+    var id: String?
+    var video: Bool?
+    var voteAverage: Int64?
+    var title: String?
+    var popularity: Double?
+    var posterPath: String?
+    var originalLanguage: String?
+    var originalTitle: String?
+    var genreIds: Array<Int64>?
+    var backdropPath: String?
+    var adult: Bool?
+    var overview: String?
+    var releaseDate: Date?
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        voteCount           <- map["vote_count"]
+        id                  <- map["id"]
+        video               <- map["video"]
+        voteAverage         <- map["vote_average"]
+        title               <- map["title"]
+        popularity          <- map["popularity"]
+        originalLanguage    <- map["original_language"]
+        originalTitle       <- map["original_title"]
+        genreIds            <- map["genre_ids"]
+        backdropPath        <- map["backdrop_path"]
+        adult               <- map["adult"]
+        overview            <- map["overview"]
+        releaseDate         <- map["release_date"]
+    }
+}
